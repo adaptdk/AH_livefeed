5ff4c9bac7464bd34d43cf9d218223fe805eaa2d
Christian Lund <christian.lund@adapt.dk>
2018-04-20 16:57:57 +0200

    changes to server

--- backend/server.go
+++ backend/server.go
@@ -8,16 +8,38 @@ package main
 
 import (
 	"flag"
-	"html/template"
 	"log"
 	"net/http"
-
+	"fmt"
+	"time"
+	"io/ioutil"
 	"github.com/gorilla/websocket"
+	"encoding/json"
 )
+
+var brands = readBrands()
+var postCodes = readPostCodes()
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options
+
+type Sale struct {
+	IconUrl string
+	Lat     string
+	Lon     string
+}
+
+type Brand struct {
+	Name    string
+	IconUrl string
+}
+
+type PostCode struct {
+	PostCode  string
+	Lat       string
+	Lon       string
+}
 
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
@@ -27,13 +49,17 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	}
 	defer c.Close()
 	for {
-		mt, message, err := c.ReadMessage()
+		time.Sleep(2 * time.Second)
+
+		sales := getProducts(1)
+		salesJson, err := json.Marshal(sales)
+
 		if err != nil {
-			log.Println("read:", err)
-			break
+			fmt.Println(err)
+			return
 		}
-		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, message)
+
+		err = c.WriteMessage(websocket.TextMessage, salesJson)
 		if err != nil {
 			log.Println("write:", err)
 			break
@@ -41,93 +67,50 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
+func getProducts(lastId int)(sales []Sale) {
+	sales = []Sale{}
+
+	mySale := Sale {
+		IconUrl: "xxx",
+		Lat:     "yyy",
+		Lon:     "zzz",
+	}
+
+	sales = append(sales, mySale)
+
+	mySale = Sale {
+		IconUrl: "dfgdfsg",
+		Lat:     "ydfgyy",
+		Lon:     "dfgdfg",
+	}
+
+	sales = append(sales, mySale)
+	return
+}
+
+func readBrands()(brands []Brand) {
+	brandJson, err := ioutil.ReadFile("brands.json")
+	if err != nil {
+		log.Print("Could not read brands data:", err)
+		return
+	}
+	json.Unmarshal(brandJson, &brands)
+	return
+}
+
+func readPostCodes()(postCodes []PostCode) {
+	postCodesJson, err := ioutil.ReadFile("zip_point.json")
+	if err != nil {
+		log.Print("Could not read postCodes data:", err)
+		return
+	}
+	json.Unmarshal(postCodesJson, &postCodes)
+	return
 }
 
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
-	http.HandleFunc("/echo", echo)
-	http.HandleFunc("/", home)
+	http.HandleFunc("/", echo)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
-
-var homeTemplate = template.Must(template.New("").Parse(`
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<script>  
-window.addEventListener("load", function(evt) {
-
-    var output = document.getElementById("output");
-    var input = document.getElementById("input");
-    var ws;
-
-    var print = function(message) {
-        var d = document.createElement("div");
-        d.innerHTML = message;
-        output.appendChild(d);
-    };
-
-    document.getElementById("open").onclick = function(evt) {
-        if (ws) {
-            return false;
-        }
-        ws = new WebSocket("{{.}}");
-        ws.onopen = function(evt) {
-            print("OPEN");
-        }
-        ws.onclose = function(evt) {
-            print("CLOSE");
-            ws = null;
-        }
-        ws.onmessage = function(evt) {
-            print("RESPONSE: " + evt.data);
-        }
-        ws.onerror = function(evt) {
-            print("ERROR: " + evt.data);
-        }
-        return false;
-    };
-
-    document.getElementById("send").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        print("SEND: " + input.value);
-        ws.send(input.value);
-        return false;
-    };
-
-    document.getElementById("close").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        ws.close();
-        return false;
-    };
-
-});
-</script>
-</head>
-<body>
-<table>
-<tr><td valign="top" width="50%">
-<p>Click "Open" to create a connection to the server, 
-"Send" to send a message to the server and "Close" to close the connection. 
-You can change the message and send multiple times.
-<p>
-<form>
-<button id="open">Open</button>
-<button id="close">Close</button>
-<p><input id="input" type="text" value="Hello world!">
-<button id="send">Send</button>
-</form>
-</td><td valign="top" width="50%">
-<div id="output"></div>
-</td></tr></table>
-</body>
-</html>
-`))
