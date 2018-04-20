1d8ecc4f37e81e9ecd557d9e990be848d2afce0a
Mikkel Jakobsen <mikkel@adapt.dk>
2018-04-20 14:22:23 +0200

    Now with marker

--- client/src/App.js
+++ client/src/App.js
@@ -21,9 +21,7 @@ const Map = compose(
   withGoogleMap
 )(props => (
   <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-    {props.isMarkerShown && (
       <Marker position={{ lat: -34.397, lng: 150.644 }} />
-    )}
   </GoogleMap>
 ));
 
