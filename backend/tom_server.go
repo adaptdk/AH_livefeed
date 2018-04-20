6c777e5f2c3048a24c0fc754ceb17b1b4f286f51
Tom Helmer Hansen <tom@adapt.dk>
2018-04-20 16:12:39 +0200

    Add sale struct

--- backend/tom_server.go
+++ backend/tom_server.go
@@ -8,16 +8,23 @@ package main
 
 import (
 	"flag"
-	"html/template"
 	"log"
 	"net/http"
-
+  "fmt"
+  "time"
 	"github.com/gorilla/websocket"
+  "encoding/json"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options
+
+type Sale struct {
+    IconUrl string
+    Lat     string
+    Lon     string
+}
 
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
@@ -27,13 +34,17 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	}
 	defer c.Close()
 	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
-		}
-		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, message)
+    time.Sleep(2 * time.Second)
+
+    sales := getProducts(1)
+    salesJson, err := json.Marshal(sales)
+
+    if err != nil {
+      fmt.Println(err)
+      return
+    }
+
+    err = c.WriteMessage(websocket.TextMessage, salesJson)
 		if err != nil {
 			log.Println("write:", err)
 			break
@@ -41,93 +52,32 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
+func getProducts(lastId int)(sales []Sale) {
+  sales = []Sale{}
+
+  mySale := Sale {
+    IconUrl: "xxx",
+    Lat:     "yyy",
+    Lon:     "zzz",
+  }
+
+  sales = append(sales, mySale)
+
+  mySale = Sale {
+    IconUrl: "dfgdfsg",
+    Lat:     "ydfgyy",
+    Lon:     "dfgdfg",
+  }
+
+  sales = append(sales, mySale)
+
+
+  return
 }
 
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
-	http.HandleFunc("/echo", echo)
-	http.HandleFunc("/", home)
+	http.HandleFunc("/", echo)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
-
-var homeTemplate = template.Must(template.New("").Parse(`
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<script>  
-window.addEventListener("load", function(evt) {
-
-    var output = document.getElementById("output");
-    var input = document.getElementById("input");
-    var ws;
-
-    var print = function(message) {
-        var d = document.createElement("div");
-        d.innerHTML = message;
-        output.appendChild(d);
-    };
-
-    document.getElementById("open").onclick = function(evt) {
-        if (ws) {
-            return false;
-        }
-        ws = new WebSocket("{{.}}");
-        ws.onopen = function(evt) {
-            print("OPEN");
-        }
-        ws.onclose = function(evt) {
-            print("CLOSE");
-            ws = null;
-        }
-        ws.onmessage = function(evt) {
-            print("RESPONSE: " + evt.data);
-        }
-        ws.onerror = function(evt) {
-            print("ERROR: " + evt.data);
-        }
-        return false;
-    };
-
-    document.getElementById("send").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        print("SEND: " + input.value);
-        ws.send(input.value);
-        return false;
-    };
-
-    document.getElementById("close").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        ws.close();
-        return false;
-    };
-
-});
-</script>
-</head>
-<body>
-<table>
-<tr><td valign="top" width="50%">
-<p>Click "Open" to create a connection to the server, 
-"Send" to send a message to the server and "Close" to close the connection. 
-You can change the message and send multiple times.
-<p>
-<form>
-<button id="open">Open</button>
-<button id="close">Close</button>
-<p><input id="input" type="text" value="Hello world!">
-<button id="send">Send</button>
-</form>
-</td><td valign="top" width="50%">
-<div id="output"></div>
-</td></tr></table>
-</body>
-</html>
-`))
